102ac52eedcd97391069aebe99c95e8b47f70817
Yeyson Cano Carbajo <Yeyson Cano Carbajo@DESKTOP-LJV5KB7>
2022-08-11 21:53:00 -0500

    Añadiendo Detalles visuales (Sebastian)

--- src/ed/grupo4/GUI/FramePrincipal.java
+++ src/ed/grupo4/GUI/FramePrincipal.java
@@ -10,10 +10,10 @@ public class FramePrincipal extends javax.swing.JFrame {
      */
     public FramePrincipal() {
         initComponents();
-        JPanel appinfo=new PanelRedondeado(20, Color.WHITE);
+        /*JPanel appinfo=new PanelRedondeado(20, Color.WHITE);
         appinfo.setBounds(480,100,300,350);
         appinfo.setOpaque(false);
-        Inicio.add(appinfo);
+        Inicio.add(appinfo);*/
     }
 
     /**
@@ -24,130 +24,53 @@ public class FramePrincipal extends javax.swing.JFrame {
     private void initComponents() {
 
         Fondo = new PanelFondo("/ed/grupo4/resources/images/Fondo.png");
-        Opciones = new javax.swing.JPanel();
-        btnInicio = new javax.swing.JLabel();
-        btnPasajeros = new javax.swing.JLabel();
-        btngestioncabinas = new javax.swing.JLabel();
-        btnabordarpasajeros = new javax.swing.JLabel();
-        btnmostrarcolas = new javax.swing.JLabel();
-        jLabel6 = new javax.swing.JLabel();
-        jLabel7 = new javax.swing.JLabel();
-        Inicio = new javax.swing.JPanel();
+        Opciones = new PanelImagen("/ed/grupo4/resources/images/Menuopc.png");
+        jPanel1 = new PanelImagen("/ed/grupo4/resources/images/inicio3.png");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setPreferredSize(new java.awt.Dimension(1280, 720));
 
         Fondo.setLayout(new java.awt.BorderLayout());
 
         Opciones.setBackground(new java.awt.Color(204, 204, 204));
-        Opciones.setPreferredSize(new java.awt.Dimension(1280, 175));
+        Opciones.setPreferredSize(new java.awt.Dimension(1280, 220));
 
-        btnInicio.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ed/grupo4/resources/images/icono_casa.png"))); // NOI18N
-        btnInicio.setToolTipText("");
-        btnInicio.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        btnInicio.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                btnInicioMouseClicked(evt);
-            }
-        });
+        jPanel1.setPreferredSize(new java.awt.Dimension(60, 60));
 
-        btnPasajeros.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ed/grupo4/resources/images/icono_casa.png"))); // NOI18N
-        btnPasajeros.setToolTipText("");
-        btnPasajeros.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        btnPasajeros.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                btnPasajerosMouseClicked(evt);
-            }
-        });
-
-        btngestioncabinas.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ed/grupo4/resources/images/icono_casa.png"))); // NOI18N
-        btngestioncabinas.setToolTipText("");
-        btngestioncabinas.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-
-        btnabordarpasajeros.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ed/grupo4/resources/images/icono_casa.png"))); // NOI18N
-        btnabordarpasajeros.setToolTipText("");
-        btnabordarpasajeros.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-
-        btnmostrarcolas.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ed/grupo4/resources/images/icono_casa.png"))); // NOI18N
-        btnmostrarcolas.setToolTipText("");
-        btnmostrarcolas.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-
-        jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ed/grupo4/resources/images/boton2.png"))); // NOI18N
-        jLabel6.setToolTipText("");
-        jLabel6.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-
-        jLabel7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ed/grupo4/resources/images/boton2.png"))); // NOI18N
-        jLabel7.setToolTipText("");
-        jLabel7.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 60, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 60, Short.MAX_VALUE)
+        );
 
         javax.swing.GroupLayout OpcionesLayout = new javax.swing.GroupLayout(Opciones);
         Opciones.setLayout(OpcionesLayout);
         OpcionesLayout.setHorizontalGroup(
             OpcionesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(OpcionesLayout.createSequentialGroup()
-                .addGap(133, 133, 133)
-                .addComponent(btnInicio)
-                .addGap(133, 133, 133)
-                .addComponent(btnPasajeros)
-                .addGap(55, 55, 55)
-                .addComponent(jLabel6)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(btngestioncabinas)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel7)
-                .addGap(55, 55, 55)
-                .addComponent(btnabordarpasajeros)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 135, Short.MAX_VALUE)
-                .addComponent(btnmostrarcolas)
-                .addGap(133, 133, 133))
+                .addGap(326, 326, 326)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(894, Short.MAX_VALUE))
         );
         OpcionesLayout.setVerticalGroup(
             OpcionesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, OpcionesLayout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(OpcionesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(btnmostrarcolas)
-                    .addComponent(btnPasajeros)
-                    .addComponent(btnInicio)
-                    .addComponent(btngestioncabinas)
-                    .addComponent(btnabordarpasajeros))
-                .addContainerGap(73, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, OpcionesLayout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGroup(OpcionesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jLabel7)
-                    .addComponent(jLabel6))
-                .addContainerGap())
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(142, 142, 142))
         );
 
         Fondo.add(Opciones, java.awt.BorderLayout.SOUTH);
-
-        Inicio.setBackground(new java.awt.Color(0, 0, 0,200));
-
-        javax.swing.GroupLayout InicioLayout = new javax.swing.GroupLayout(Inicio);
-        Inicio.setLayout(InicioLayout);
-        InicioLayout.setHorizontalGroup(
-            InicioLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 1280, Short.MAX_VALUE)
-        );
-        InicioLayout.setVerticalGroup(
-            InicioLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 545, Short.MAX_VALUE)
-        );
-
-        Fondo.add(Inicio, java.awt.BorderLayout.CENTER);
 
         getContentPane().add(Fondo, java.awt.BorderLayout.CENTER);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void btnPasajerosMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnPasajerosMouseClicked
-        ((PanelFondo)Fondo).moverLogoAEsquinaIzquierda();
-    }//GEN-LAST:event_btnPasajerosMouseClicked
-
-    private void btnInicioMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnInicioMouseClicked
-        ((PanelFondo)Fondo).moverLogoAlCentro();
-    }//GEN-LAST:event_btnInicioMouseClicked
 
     /**
      * @param args the command line arguments
@@ -185,14 +108,7 @@ public class FramePrincipal extends javax.swing.JFrame {
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel Fondo;
-    private javax.swing.JPanel Inicio;
     private javax.swing.JPanel Opciones;
-    private javax.swing.JLabel btnInicio;
-    private javax.swing.JLabel btnPasajeros;
-    private javax.swing.JLabel btnabordarpasajeros;
-    private javax.swing.JLabel btngestioncabinas;
-    private javax.swing.JLabel btnmostrarcolas;
-    private javax.swing.JLabel jLabel6;
-    private javax.swing.JLabel jLabel7;
+    private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
