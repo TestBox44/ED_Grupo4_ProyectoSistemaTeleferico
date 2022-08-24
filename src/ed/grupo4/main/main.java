88c2c9613742acc545bda6611d1207adaa699e20
TestBox44 <iceglace12@hotmail.com>
2022-08-23 20:55:50 -0500

    mas y mas ventanas

--- src/ed/grupo4/main/main.java
+++ src/ed/grupo4/main/main.java
@@ -8,10 +8,10 @@ import ed.grupo4.util.Dijkstra;
  * @author Yeyson Samir Cano
  * @author Richard Saul Alvarez
  */
+
 public class main {
     public static void main(String[] args) {
         FramePrincipal principal = new FramePrincipal();
         principal.setVisible(true);
-        
     }
 }
---
+++ src/ed/grupo4/util/UtilidadesDeGrafo.java
@@ -0,0 +1,13 @@
+package ed.grupo4.util;
+
+public class UtilidadesDeGrafo {
+    public static final int max=1000;
+    public static final int pesos[][]={{max,3,1,max,max,max,max,max},
+                                        {3,max,5,1,2,max,max,max},
+                                        {1,5,max,2,max,2,max,max},
+                                        {max,1,2,max,3,1,max,max},
+                                        {max,2,max,3,max,4,max,9},
+                                        {max,max,2,1,4,max,5,7},
+                                        {max,max,max,max,max,5,max,3},
+                                        {max,max,max,max,9,7,3,max}};
+}
